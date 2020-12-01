bdaa4ff6c8c7c43583ea40932f4510ee0c729d53
Patricia Selpa <patricia.selpa@gmail.com>
2020-12-01 20:23:28 -0300

    Criado o repositório, projeto inicial e adicionado o gitignore.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace seu_vizinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
