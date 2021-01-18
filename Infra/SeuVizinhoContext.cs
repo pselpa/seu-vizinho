8b2f64a407a40e1168f947c7b694bb1189443518
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2021-01-18 19:23:50 -0300

    Iniciado Infra criado pastas para mapping, criado docker-compose.yml e baixado pacotes para iniciar migrations

---
+++ Infra/SeuVizinhoContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Users;
+using Domain.Rents;
+using Domain.Products;
+using System.Reflection;
+
+namespace Infra
+{
+    public class SeuVizinhoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Override pois estamos sobrescrevendo o comportamento padrão.
+
+        protected override void OnConfiguring(DbContextBuilder optionsBuilder)
+
+        // Initial Catalog = nome do banco de dados que será criado
+        // PWD = Password
+        optionsBuilder.UseSqlServer("Data Source=localhost;User Id=sa;PWD=some(!)Password;Initial Catalog=SeuVizinho");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /* Nesta linha estamos informando ao EF de onde
+        ele irá ler as configurações de mapeamento das entidades */
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            Assembly.GetExecutingAssembly()
+        );
+    }
+}
---
+++ Infra/UserMapping.cs
@@ -0,0 +1,27 @@
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .Property(user => user.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder
+                .Property(user => user.Email)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder
+                .Property(user => user.Password)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
