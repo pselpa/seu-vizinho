57c6c27774eb2f4287a9eb2d67ca5f38d7ab18c3
Patricia Selpa <patricia.selpa@gmail.com>
2021-01-25 22:42:09 -0300

    Alterada a Validação de produto. Testes atualizados.

--- Tests/ProductTest.cs
+++ Tests/ProductTest.cs
@@ -9,9 +9,10 @@ namespace Tests
         [Theory]
         [InlineData(null)]
         [InlineData("")]
+        [InlineData("F")]
         public void Should_return_false_when_name_is_invalid(string name)
         {
-            var product = new Product(name, "Chave de fenda n° 2", "", "", "voltage", "frequency", 5, 50);
+            var product = new Product(name, "Chave de fenda n° 2", "", "", "", "", "", null, 0, null, null, null, null);
 
             var productIsValid = product.Validate().isValid;
 
@@ -23,7 +24,7 @@ namespace Tests
         [InlineData("")]
         public void Should_return_false_when_description_is_invalid(string description)
         {
-            var product = new Product("chave de fenda", description, "", "", "voltage", "frequency", 5, 50);
+            var product = new Product("chave de fenda", description, "", "", "", "", "", null, 0, null, null, null, null);
 
             var productIsValid = product.Validate().isValid;
 
@@ -31,53 +32,16 @@ namespace Tests
         }
 
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public void Should_return_false_when_voltage_is_invalid(string voltage)
+        [InlineData(null, null, null, null, null, null, null, null, null, null)]
+        [InlineData(null, null, null, null, null, 0, 0, 0, 0, 0)]
+        [InlineData("", "", "", "", "", 0, 0, 0, 0, 0)]
+        public void Should_return_true_when_nonobligatory_items_are_null_empty_or_zero(string accessories, string brand, string model, string voltage, string frequency, double pricePerHour, double pricePerDayByWeek, double pricePerDayByBiweekly, double pricePerDayByMonth, int rentingPeriodLimit)
         {
-            var product = new Product("chave de fenda", "Chave de fenda n° 2", "", "", voltage, "frequency", 5, 50);
-
-            var productIsValid = product.Validate().isValid;
-
-            Assert.False(productIsValid);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public void Should_return_false_when_frequency_is_invalid(string frequency)
-        {
-            var product = new Product("chave de fenda", "Chave de fenda n° 2", "", "", "voltage", frequency, 5, 50);
-
-            var productIsValid = product.Validate().isValid;
-
-            Assert.False(productIsValid);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData(0)]
-        public void Should_return_false_when_quantity_is_invalid(int quantity)
-        {
-            var product = new Product("chave de fenda", "Chave de fenda n° 2", "", "", "voltage", "0", quantity, 50);
-
-            var productIsValid = product.Validate().isValid;
-
-            Assert.False(productIsValid);
-        }
-
-        [Theory]
-        [InlineData(null, null, null)]
-        [InlineData(null, null, 0)]
-        [InlineData("", "", 0)]
-        public void Should_return_true_when_nonobligatory_items_are_null_empty_or_zero(string brand, string model, int rentingPeriodLimit)
-        {
-            var product = new Product("chave de fenda", "Chave de fenda n° 2", brand, model, "voltage", "frequency", 5, rentingPeriodLimit);
+            var product = new Product("chave de fenda", "Chave de fenda n° 2", accessories, brand, model, voltage, frequency, pricePerHour, 0, pricePerDayByWeek, pricePerDayByBiweekly, pricePerDayByMonth, rentingPeriodLimit);
 
             var productIsValid = product.Validate().isValid;
 
             Assert.True(productIsValid);
-        
         }
     }
 }
