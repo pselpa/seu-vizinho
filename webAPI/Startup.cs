94d6b9f2d11c5a35cb3d29f13f85d93ee363154e
Patricia Selpa <patricia.selpa@gmail.com>
2021-02-04 21:42:47 -0300

    Alterações em Startup.cs

--- webAPI/Startup.cs
+++ webAPI/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Common;
+using Domain.Users;
+using Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +30,26 @@ namespace webAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("any",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin();
+                    }
+                );
+            });
 
             services.AddControllers();
+
+            // services.AddSingleton(typeof (IRepository<>), typeof (RepositoryInMemory<>));
+            services.AddScoped(typeof (IRepository<>), typeof (Repository<>));
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<IUsersService, UsersService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "webAPI", Version = "v1" });
@@ -37,6 +59,14 @@ namespace webAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("any");
+            using (var db = new SeuVizinhoContext())
+            {
+                // Este comando irá criar o banco de dados (quando ele ainda não existir)
+                // Também executará todas as migrations e seeders
+                db.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
